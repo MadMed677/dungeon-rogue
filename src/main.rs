a781613e5264e1b7971a1989cc304cd8aa558597
Artem Anashev <madmed677@gmail.com>
2022-04-20 23:11:03 +0400

    Add bevy_ecs_ldtk crate to spawn the map

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-mod map;
 mod player;
 
 use bevy::prelude::*;
-use bevy_ecs_tilemap::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+// use bevy_ecs_tilemap::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
-use map::MapPlugin;
+// use map::MapPlugin;
 use player::PlayerPlugin;
 
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
@@ -30,12 +30,37 @@ fn setup(mut commands: Commands) {
     // });
 }
 
+#[derive(Default, Component)]
+struct ComponentA;
+
+#[derive(Default, Component)]
+struct ComponentB;
+
+#[derive(Bundle, LdtkEntity)]
+pub struct MyBundle {
+    a: ComponentA,
+    b: ComponentB,
+
+    #[sprite_sheet_bundle]
+    #[bundle]
+    sprite_bundle: SpriteSheetBundle,
+}
+
+fn setup_ldtk(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    commands.spawn_bundle(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("test.ldtk"),
+        ..Default::default()
+    });
+}
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .insert_resource(WindowDescriptor {
             title: String::from("Dungeon Rogue"),
-            width: 700.0,
+            width: 800.0,
             height: 680.0,
             ..Default::default()
         })
@@ -44,8 +69,10 @@ fn main() {
         .add_startup_system(setup)
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
-        .add_plugin(TilemapPlugin)
+        .add_plugin(LdtkPlugin)
+        .add_startup_system(setup_ldtk)
+        .insert_resource(LevelSelection::Index(0))
+        .register_ldtk_entity::<MyBundle>("MyEntityIdentifier")
         .add_plugin(PlayerPlugin)
-        .add_plugin(MapPlugin)
         .run();
 }
