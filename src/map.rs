e3642d76d2effc062dc540503d5a6357f145e8fa
Artem Anashev <madmed677@gmail.com>
2022-05-05 23:08:53 +0400

    Move wall collisions to the map system

--- src/map.rs
+++ src/map.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 use bevy_inspector_egui::Inspectable;
@@ -66,19 +68,134 @@ fn fit_camera_inside_current_level(
     };
 }
 
-fn _spawn_wall_collision(
-    mut _commands: Commands,
-    map_query: Query<(Entity, &GridCoords, &Parent), Added<Wall>>,
+fn spawn_wall_collision(
+    mut commands: Commands,
+    wall_query: Query<(&GridCoords, &Parent), Added<Wall>>,
+    parent_query: Query<&Parent, Without<Wall>>,
+    level_query: Query<(Entity, &Handle<LdtkLevel>)>,
+    levels: Res<Assets<LdtkLevel>>,
 ) {
-    for (entity, grid_coords, _parent) in map_query.iter() {
-        println!("Grid coords: {:?}", grid_coords);
+    #[derive(Clone, Copy, PartialEq, Eq, Debug, Default, Hash)]
+    struct Plate {
+        left: i32,
+        right: i32,
+    }
+    // Store all GridCoords by specific level
+    let mut level_to_wall_locations: HashMap<Entity, HashSet<GridCoords>> = HashMap::new();
+    for (&grid_coords, &Parent(parent)) in wall_query.iter() {
+        if let Ok(&Parent(level_entity)) = parent_query.get(parent) {
+            level_to_wall_locations
+                .entry(level_entity)
+                .or_insert(HashSet::new())
+                .insert(grid_coords);
+        }
+    }
 
-        // let collider = ColliderBundle {
-        //     shape: ColliderShape::cuboid(100.0, 0.1).into(),
-        //     ..Default::default()
-        // };
+    if !wall_query.is_empty() {
+        for (level_entity, level_handle) in level_query.iter() {
+            if let Some(level_walls) = level_to_wall_locations.get(&level_entity) {
+                let level = levels
+                    .get(level_handle)
+                    .expect("Level should be loaded by this point");
 
-        // commands.entity(entity).insert_bundle(collider);
+                let LayerInstance {
+                    c_wid: width,
+                    c_hei: height,
+                    grid_size,
+                    ..
+                } = level
+                    .level
+                    .layer_instances
+                    .clone()
+                    .expect("Level asset should have layers")[0];
+
+                // Combine wall tiles into flat "plates" in each individual row
+                let mut plate_stack: Vec<Vec<Plate>> = Vec::new();
+
+                for y in 0..height {
+                    let mut row_plates: Vec<Plate> = Vec::new();
+                    let mut plate_start = None;
+
+                    // + 1 to the width so the algorithm "terminates" plates that touch the right
+                    // edge
+                    for x in 0..width + 1 {
+                        match (plate_start, level_walls.contains(&GridCoords { x, y })) {
+                            (Some(s), false) => {
+                                row_plates.push(Plate {
+                                    left: s,
+                                    right: x - 1,
+                                });
+
+                                plate_start = None;
+                            }
+                            (None, true) => plate_start = Some(x),
+                            _ => (),
+                        }
+                    }
+
+                    plate_stack.push(row_plates);
+                }
+
+                // combine "plates" into rectangles across multiple rows
+                let mut wall_rects: Vec<Rect<i32>> = Vec::new();
+                let mut previous_rects: HashMap<Plate, Rect<i32>> = HashMap::new();
+
+                // an extra empty row so the algorithm "terminates" the rects that
+                // touch the top edge
+                plate_stack.push(Vec::new());
+
+                for (y, row) in plate_stack.iter().enumerate() {
+                    let mut current_rects: HashMap<Plate, Rect<i32>> = HashMap::new();
+
+                    for plate in row {
+                        if let Some(previous_rect) = previous_rects.remove(plate) {
+                            current_rects.insert(
+                                *plate,
+                                Rect {
+                                    top: previous_rect.top + 1,
+                                    ..previous_rect
+                                },
+                            );
+                        } else {
+                            current_rects.insert(
+                                *plate,
+                                Rect {
+                                    top: y as i32,
+                                    bottom: y as i32,
+                                    left: plate.left,
+                                    right: plate.right,
+                                },
+                            );
+                        }
+                    }
+
+                    wall_rects.append(&mut previous_rects.values().copied().collect());
+                    previous_rects = current_rects;
+                }
+
+                for wall_rect in wall_rects {
+                    commands
+                        .spawn()
+                        .insert(Collider::cuboid(
+                            (wall_rect.right as f32 - wall_rect.left as f32 + 1.0)
+                                * grid_size as f32
+                                / 2.0,
+                            (wall_rect.top as f32 - wall_rect.bottom as f32 + 1.0)
+                                * grid_size as f32
+                                / 2.0,
+                        ))
+                        .insert(RigidBody::Fixed)
+                        .insert(Friction::new(0.1))
+                        .insert(Transform::from_xyz(
+                            (wall_rect.left + wall_rect.right + 1) as f32 * grid_size as f32 / 2.0,
+                            (wall_rect.bottom + wall_rect.top + 1) as f32 * grid_size as f32 / 2.0,
+                            0.0,
+                        ))
+                        .insert(GlobalTransform::default())
+                        .insert(Parent(level_entity));
+                }
+            }
+        }
     }
 }
 
@@ -89,8 +206,9 @@ impl Plugin for MapPlugin {
         app.add_plugin(LdtkPlugin)
             .add_startup_system(setup)
             .add_system(fit_camera_inside_current_level)
-            // .add_system(spawn_wall_collision)
+            .add_system(spawn_wall_collision)
             .insert_resource(LevelSelection::Index(0))
-            .register_ldtk_int_cell::<WallBundle>(1);
+            .register_ldtk_int_cell::<WallBundle>(1)
+            .register_ldtk_int_cell::<WallBundle>(3);
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -1,10 +1,8 @@
 use bevy::prelude::*;
-use bevy_ecs_ldtk::prelude::*;
 use bevy_inspector_egui::Inspectable;
 use bevy_rapier2d::prelude::*;
 
-use crate::{map::Wall, Speed};
-use std::collections::{HashMap, HashSet};
+use crate::Speed;
 
 #[derive(Component, Inspectable)]
 pub struct Player;
@@ -15,7 +13,6 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(spawn_player)
             .add_startup_system(spawn_floor)
-            .add_system(player_collision)
             .add_system(player_movement);
     }
 }
@@ -61,7 +58,7 @@ fn spawn_player(mut commands: Commands, mut rapier_config: ResMut<RapierConfigur
                 custom_size: Some(Vec2::new(sprite_size, sprite_size)),
                 ..Default::default()
             },
-            transform: Transform::from_xyz(x, y, 2.0),
+            transform: Transform::from_xyz(x, y, 3.0),
             ..Default::default()
         })
         .insert(Player)
@@ -92,152 +89,5 @@ fn player_movement(
 
         velocity.linvel = Vec2::new(direction_x * speed.0, direction_y * speed.0);
         velocity.angvel = 0.0;
-    }
-}
-
-fn player_collision(
-    mut commands: Commands,
-    player_query: Query<&Transform, With<Player>>,
-    wall_query: Query<(&GridCoords, &Parent), Added<Wall>>,
-    parent_query: Query<&Parent, Without<Wall>>,
-    level_query: Query<(Entity, &Handle<LdtkLevel>)>,
-    levels: Res<Assets<LdtkLevel>>,
-) {
-    #[derive(Clone, Copy, PartialEq, Eq, Debug, Default, Hash)]
-    struct Plate {
-        left: i32,
-        right: i32,
-    }
-
-    let player = player_query.get_single();
-
-    if let Ok(_player_transform) = player {
-        // for grid_coords in wall_query.iter() {
-        //     if player_transform.translation.x as i32 == grid_coords.x
-        //         && player_transform.translation.y as i32 == grid_coords.y
-        //     {
-        //         println!("Collision has been detected!");
-        //     }
-        // }
-
-        // Store all GridCoords by specific level
-        let mut level_to_wall_locations: HashMap<Entity, HashSet<GridCoords>> = HashMap::new();
-        for (&grid_coords, &Parent(parent)) in wall_query.iter() {
-            if let Ok(&Parent(level_entity)) = parent_query.get(parent) {
-                level_to_wall_locations
-                    .entry(level_entity)
-                    .or_insert(HashSet::new())
-                    .insert(grid_coords);
-            }
-        }
-
-        if !wall_query.is_empty() {
-            for (level_entity, level_handle) in level_query.iter() {
-                if let Some(level_walls) = level_to_wall_locations.get(&level_entity) {
-                    let level = levels
-                        .get(level_handle)
-                        .expect("Level should be loaded by this point");
-
-                    let LayerInstance {
-                        c_wid: width,
-                        c_hei: height,
-                        grid_size,
-                        ..
-                    } = level
-                        .level
-                        .layer_instances
-                        .clone()
-                        .expect("Level asset should have layers")[0];
-
-                    // Combine wall tiles into flat "plates" in each individual row
-                    let mut plate_stack: Vec<Vec<Plate>> = Vec::new();
-
-                    for y in 0..height {
-                        let mut row_plates: Vec<Plate> = Vec::new();
-                        let mut plate_start = None;
-
-                        // + 1 to the width so the algorithm "terminates" plates that touch the right
-                        // edge
-                        for x in 0..width + 1 {
-                            match (plate_start, level_walls.contains(&GridCoords { x, y })) {
-                                (Some(s), false) => {
-                                    row_plates.push(Plate {
-                                        left: s,
-                                        right: x - 1,
-                                    });
-
-                                    plate_start = None;
-                                }
-                                (None, true) => plate_start = Some(x),
-                                _ => (),
-                            }
-                        }
-
-                        plate_stack.push(row_plates);
-                    }
-
-                    // combine "plates" into rectangles across multiple rows
-                    let mut wall_rects: Vec<Rect<i32>> = Vec::new();
-                    let mut previous_rects: HashMap<Plate, Rect<i32>> = HashMap::new();
-
-                    // an extra empty row so the algorithm "terminates" the rects that
-                    // touch the top edge
-                    plate_stack.push(Vec::new());
-
-                    for (y, row) in plate_stack.iter().enumerate() {
-                        let mut current_rects: HashMap<Plate, Rect<i32>> = HashMap::new();
-
-                        for plate in row {
-                            if let Some(previous_rect) = previous_rects.remove(plate) {
-                                current_rects.insert(
-                                    *plate,
-                                    Rect {
-                                        top: previous_rect.top + 1,
-                                        ..previous_rect
-                                    },
-                                );
-                            } else {
-                                current_rects.insert(
-                                    *plate,
-                                    Rect {
-                                        top: y as i32,
-                                        bottom: y as i32,
-                                        left: plate.left,
-                                        right: plate.right,
-                                    },
-                                );
-                            }
-                        }
-
-                        wall_rects.append(&mut previous_rects.values().copied().collect());
-                        previous_rects = current_rects;
-                    }
-
-                    for wall_rect in wall_rects {
-                        commands
-                            .spawn()
-                            .insert(Collider::cuboid(
-                                (wall_rect.right as f32 - wall_rect.left as f32 + 1.0)
-                                    * grid_size as f32
-                                    / 2.0,
-                                (wall_rect.top as f32 - wall_rect.bottom as f32 + 1.0)
-                                    * grid_size as f32
-                                    / 2.0,
-                            ))
-                            .insert(RigidBody::Fixed)
-                            .insert(Friction::new(0.1))
-                            .insert(Transform::from_xyz(
-                                (wall_rect.left + wall_rect.right + 1) as f32 * grid_size as f32
-                                    / 2.0,
-                                (wall_rect.bottom + wall_rect.top + 1) as f32 * grid_size as f32
-                                    / 2.0,
-                                0.0,
-                            ))
-                            .insert(GlobalTransform::default())
-                            .insert(Parent(level_entity));
-                    }
-                }
-            }
-        }
     }
 }
