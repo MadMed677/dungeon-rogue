2fc727f64b3e5c829b1d926315e609f9505122b8
Artem Anashev <madmed677@gmail.com>
2022-05-20 12:32:02 +0400

    Add debug layer for `Climbable` components

--- src/debug.rs
+++ src/debug.rs
@@ -7,9 +7,7 @@ use bevy_inspector_egui::{Inspectable, InspectorPlugin, RegisterInspectable};
 
 use crate::map::WallCollision;
 use crate::player::Player;
-use crate::Speed;
-
-use rand::{thread_rng, Rng};
+use crate::{Climbable, Speed};
 
 pub struct DebugPlugin;
 
@@ -21,12 +19,15 @@ struct Inspector {
 
 impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
+        // label for our debug stage
+        static DEBUG: &str = "debug";
+
         if cfg!(debug_assertions) {
             app.add_plugin(InspectorPlugin::<Inspector>::new())
-                // app.add_plugin(WorldInspectorPlugin::new())
                 .register_inspectable::<Player>()
                 .register_inspectable::<Speed>()
-                .add_system(debug_collisions)
+                .add_stage_after(CoreStage::Update, DEBUG, SystemStage::single_threaded())
+                .add_system_to_stage(DEBUG, debug_collisions)
                 .add_plugin(LogDiagnosticsPlugin::default())
                 .add_plugin(FrameTimeDiagnosticsPlugin::default())
                 .add_plugin(RapierDebugRenderPlugin::default());
@@ -38,72 +39,83 @@ impl Plugin for DebugPlugin {
 ///  the collision to be able to debug it
 fn debug_collisions(
     mut commands: Commands,
-    wall_colliders: Query<(Entity, &Collider, &GlobalTransform), Added<WallCollision>>,
-    player_collider: Query<(Entity, &Collider, &Transform), With<Player>>,
+    // wall_colliders: Query<(Entity, &Collider, &GlobalTransform), With<WallCollision>>,
+    player_collider: Query<(Entity, &Collider, &GlobalTransform), Added<Player>>,
+    climbables_collider: Query<(Entity, &Collider, &GlobalTransform), Added<Climbable>>,
 ) {
     // Show debug layer for the walls
-    for (entity, collider, transform) in wall_colliders.iter() {
-        let half_sizes = collider.as_cuboid().unwrap().half_extents();
-        let full_sizes = half_sizes * 2.0;
+    // for (entity, collider, transform) in wall_colliders.iter() {
+    //     let half_sizes = collider.as_cuboid().unwrap().half_extents();
+    //     let full_sizes = half_sizes * 2.0;
 
-        let mut rng = thread_rng();
-
-        // Make different colors for different colliders
-        let red = rng.gen_range(0.0..1.0);
-        let green = rng.gen_range(0.0..1.0);
-        let blue = rng.gen_range(0.0..1.0);
-
-        commands.entity(entity).insert_bundle(SpriteBundle {
-            sprite: Sprite {
-                color: Color::rgba(red, green, blue, 0.5),
-                custom_size: Some(full_sizes),
-                ..Default::default()
-            },
-            transform: Transform {
-                translation: Vec3::new(transform.translation.x, transform.translation.y, 20.0),
-                rotation: transform.rotation,
-                scale: transform.scale,
-            },
-            ..Default::default()
-        });
-    }
+    //     commands
+    //         .spawn()
+    //         .insert_bundle(SpriteBundle {
+    //             sprite: Sprite {
+    //                 color: Color::rgba(0.5, 0.5, 0.5, 0.5),
+    //                 custom_size: Some(full_sizes),
+    //                 ..Default::default()
+    //             },
+    //             transform: Transform {
+    //                 translation: Vec3::new(transform.translation.x, transform.translation.y, 20.0),
+    //                 rotation: transform.rotation,
+    //                 scale: transform.scale,
+    //             },
+    //             ..Default::default()
+    //         })
+    //         .insert(Parent(entity));
+    // }
 
     // Show the debug layer for the player
-    //
-    // !!Note!! This code will add SpriteBundle on each tick
-    //  it happenes because Added<Player> don't use properly
-    //  because the Player is a composite entity
-    //
-    // We will create the Player via `register_ldtk_entity` and
-    //  create an empty Player entity with Worldly coords
-    //  and after that we use `spawn_player` system which will add
-    //  an extra components to the entity to make it work properly
-    //
-    // I don't know how to deal it better and use `Added<Player>` but
-    //  only when `spawn_player` added all required components
-    //  because of that I made just `With<Player>` and call this system
-    //  on each tick. It's definitely bad for performance but I think
-    //  it's okay for the debug mode. I'll handle it later (I hope so)
     if let Ok((player_entity, player_collider, player_transform)) = player_collider.get_single() {
         let half_sizes = player_collider.as_cuboid().unwrap().half_extents();
         let full_sizes = half_sizes * 2.0;
 
-        commands.entity(player_entity).insert_bundle(SpriteBundle {
+        let debug_player_layer = commands.spawn_bundle(SpriteBundle {
             sprite: Sprite {
                 color: Color::rgba(0.5, 0.5, 0.5, 0.5),
                 custom_size: Some(full_sizes),
                 ..Default::default()
             },
             transform: Transform {
-                translation: Vec3::new(
-                    player_transform.translation.x,
-                    player_transform.translation.y,
-                    20.0,
-                ),
+                // Create relative coordinates for the player
+                translation: Vec3::new(0.0, 0.0, 20.0),
                 rotation: player_transform.rotation,
                 scale: player_transform.scale,
             },
             ..Default::default()
         });
+
+        let debug_player_layer_entity = debug_player_layer.id();
+
+        // Add Debug Player layer as a children of the player itself
+        commands
+            .entity(player_entity)
+            .add_child(debug_player_layer_entity);
+    }
+
+    for (climbable_entity, climbable_collider, climbable_transform) in climbables_collider.iter() {
+        let half_sizes = climbable_collider.as_cuboid().unwrap().half_extents();
+        let full_sizes = half_sizes * 2.0;
+
+        let debug_climbable_layer = commands.spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgba(0.7, 0.7, 0.7, 0.5),
+                custom_size: Some(full_sizes),
+                ..Default::default()
+            },
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 20.0),
+                rotation: climbable_transform.rotation,
+                scale: climbable_transform.scale,
+            },
+            ..Default::default()
+        });
+
+        let debug_climbable_layer_entity = debug_climbable_layer.id();
+
+        commands
+            .entity(climbable_entity)
+            .add_child(debug_climbable_layer_entity);
     }
 }
